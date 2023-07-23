721f3cbc86900ff3b27674710bcd6dac40b57a38
Roderick <fonchamroderick555@gmail.com>
2023-07-23 21:10:17 +0100

    change from branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit and save to reload.
+          Edit and save to reload. Added this just for fun...
         </p>
         <a
           className="App-link"
